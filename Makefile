369778c61336f2c5f2e517476695c9982bead773
Sardorkhuja <mr.khuzha@gmail.com>
2021-06-22 18:37:46 +0300

    add == operator

--- Makefile
+++ Makefile
@@ -1,3 +1,2 @@
 all:
 	g++ main.cpp srcs/*.cpp srcs/operators/*.cpp
-	
--- srcs/operators/comparison.cpp
+++ srcs/operators/comparison.cpp
@@ -3,6 +3,25 @@
 #include "../BigInteger.h"
 
 
+bool BigInteger::operator == (BigInteger num2obj)
+{
+  std::vector<int> num1 = this->getValue();
+  std::vector<int> num2 = num2obj.getValue();
+  int len1 = num1.size();
+  int len2 = num2.size();
+
+  if (len1 != len2)
+    return false;
+
+  for (int i = num1.size() - 1; i >= 0; i--)
+  {
+    if (num1[i] != num2[i])
+      return false;
+  }
+
+  return true;
+}
+
 bool BigInteger::operator > (BigInteger num2obj)
 {
   std::vector<int> num1 = this->getValue();
@@ -37,5 +56,5 @@ bool BigInteger::operator > (BigInteger num2obj)
 
 bool BigInteger::operator < (BigInteger num2obj)
 {
-  return !(*this > num2obj);
+  return !(*this > num2obj) && !(*this == num2obj);
 }
